fa877eeb63953bb299bad3698b669ac1b5583402
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-10-17 16:10:28 -0400

    Add files via upload

---
+++ JavaScript/Button Click/script.js
@@ -0,0 +1,14 @@
+function changeText(element) {
+  element.innerText='Logout';
+}
+changeText();
+
+function hide(a) {
+  a.remove();
+}
+hide();
+
+function liked(x) {
+  alert('Ninja was liked')
+}
+liked();
