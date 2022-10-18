2ee24ced4bdc856c58593ac1aaa9aaf0520c4709
Artan Merko <97398977+artanmerko@users.noreply.github.com>
2022-10-18 19:59:31 -0400

    Add files via upload

---
+++ JavaScript/Hover Event/script.js
@@ -0,0 +1,13 @@
+function addShadow(element){
+  element.classList.add('shadow');
+}
+addShadow();
+function removeShadow(element){
+  element.classList.remove('shadow');
+}
+removeShadow();
+
+function hide(e){
+  e.remove()
+}
+hide()
